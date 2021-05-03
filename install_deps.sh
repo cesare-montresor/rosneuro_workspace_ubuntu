a6c646246c6e92e90ca92e3e3505e82e6a3c6884
Cesare Montresor <cesare.montresor@gmail.com>
2021-05-03 17:41:49 +0200

    Update install_deps.sh
    updates

--- install_deps.sh
+++ install_deps.sh
@@ -1,9 +1,29 @@
 #! /bin/bash
 
 sudo pwd
-
 export PROJECT_ROOT=$(pwd)
+
+
+# Acquisition plugins
+sudo apt-get install libeegdev-dev 
 cd deps/; sudo dpkg -i liblsl-1.13.1-Linux64-bionic.deb
+
+
+# visualizaton
+wget -O- http://neuro.debian.net/lists/bionic.de-m.full | sudo tee /etc/apt/sources.list.d/neurodebian.sources.list
+sudo apt-key adv --recv-keys --keyserver hkp://pool.sks-keyservers.net:80 0xA5D32F012649A5A9
+sudo apt-get update
+
+sudo apt-get install libxdffileio-dev   
+sudo apt-get install librtfilter-dev
+
+
+cd deps/mcpanel/; sudo -H install.sh
+cd $PROJECT_ROOT
+
+
+# wtklibs
+sudo apt-get install libnfft3-dev libfftw3-dev libtinyxml2-dev
 
 cd deps/wtkcore/; sudo -H install.sh
 cd $PROJECT_ROOT
